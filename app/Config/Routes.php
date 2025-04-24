4faa7a45bfccdded11ab56b8d36ac3548ae6ab9a
ramadanramad <alexanderramadan2@gmail.com>
2025-04-24 22:56:37 +0700

    First commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,20 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/*
+ * --------------------------------------------------------------------
+ * Route Definitions
+ * --------------------------------------------------------------------
+ */
+$routes->setDefaultController('Register');
+$routes->get('/', 'Register::index', ['filter' => 'guestFilter']);
+$routes->get('/register', 'Register::index', ['filter' => 'guestFilter']);
+$routes->post('/register', 'Register::register', ['filter' => 'guestFilter']);
+ 
+$routes->get('/login', 'Login::index', ['filter' => 'guestFilter']);
+$routes->post('/login', 'Login::authenticate', ['filter' => 'guestFilter']);
+ 
+$routes->get('/logout', 'Login::logout', ['filter' => 'authFilter']);
+$routes->get('/dashboard', 'Dashboard::index', ['filter' => 'authFilter']);
+
---
+++ app/Controllers/Register.php
@@ -0,0 +1,44 @@
+<?php
+ 
+namespace App\Controllers;
+ 
+use App\Controllers\BaseController;
+use App\Models\UserModel;
+ 
+class Register extends BaseController
+{
+ 
+    public function __construct(){
+        helper(['form']);
+    }
+ 
+    public function index()
+    {
+        $data = [];
+        return view('register', $data);
+    }
+   
+    public function register()
+    {
+        $rules = [
+            'email' => ['rules' => 'required|min_length[4]|max_length[255]|valid_email|is_unique[users.email]'],
+            'password' => ['rules' => 'required|min_length[8]|max_length[255]'],
+            'confirm_password'  => [ 'label' => 'confirm password', 'rules' => 'matches[password]']
+        ];
+           
+ 
+        if($this->validate($rules)){
+            $model = new UserModel();
+            $data = [
+                'email'    => $this->request->getVar('email'),
+                'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT)
+            ];
+            $model->save($data);
+            return redirect()->to('/login');
+        }else{
+            $data['validation'] = $this->validator;
+            return view('register', $data);
+        }
+           
+    }
+}
